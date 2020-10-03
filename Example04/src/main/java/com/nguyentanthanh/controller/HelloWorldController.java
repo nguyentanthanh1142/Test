1e0d4e4c0f88f3a258076e57519d25b327b1ee18
HP <HP@DESKTOP-UUMSTEN>
2020-10-03 18:01:15 +0700

    thu 3

---
+++ Example04/src/main/java/com/nguyentanthanh/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.nguyentanthanh.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloWorldController {
+	String message = "Welcome to Spring MVC";
+
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
